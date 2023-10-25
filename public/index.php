e26b2e80f011d4570670bba5a31f4c4f7f0c1d3a
李宣佑 <3B032106@gm.student.ncut.edu.tw>
2023-10-25 11:51:25 +0800

    Add composer monolog/monolog

--- public/index.php
+++ public/index.php
@@ -5,9 +5,23 @@ use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
 
-$world = new World();
 $lara= new Lara();
 $vincent= new Hello\Someone('Vincent');
 
 $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
+
+$world = new World();
+
+//monolog/monolog
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('WISD');
+$log->pushHandler(new StreamHandler('../Log/my.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
